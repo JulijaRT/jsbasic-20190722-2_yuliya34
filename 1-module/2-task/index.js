ca7b38b6f6f3ba6833b9007a2ce5b8030421e6b4
JulijaRT <53298016+JulijaRT@users.noreply.github.com>
2019-07-29 12:38:01 +0300

    Update index.js
    Функция валидации имени

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  
+	if (name.length == 0 || name.length < 4 || ~name.indexOf(" ")) {
+  return false;
+	}
+	return true;
 }
 
 function sayHello() {
